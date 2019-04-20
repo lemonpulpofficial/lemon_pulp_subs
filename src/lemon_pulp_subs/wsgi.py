857d195e3f41584c36b0fd90c742acd4eba670ab
markmysourcecode <markusemaibox@gmail.com>
2019-04-21 02:23:09 +0800

    initial commit

---
+++ src/lemon_pulp_subs/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lemon_pulp_subs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lemon_pulp_subs.settings')
+
+application = get_wsgi_application()
